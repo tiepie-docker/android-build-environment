5c96affce376372b956b142b4852c3d0e97794a6
Dennis IJsebaart <d.ijsebaart@tiepie.nl>
2017-10-18 16:04:58 +0200

    Redused verbosity

--- update.sh
+++ update.sh
@@ -63,7 +63,7 @@ sudo chown -R "$(id -u):$(id -g)" "$dir"
 xz -d < $dir/rootfs.tar.xz | gzip -c > $dir/rootfs.tar.gz
 sed -i /^ENV/d "${dir}/Dockerfile"
 echo "ENV ARCH=${UNAME_ARCH} UBUNTU_SUITE=${SUITE} DOCKER_REPO=${DOCKER_REPO}" >> "${dir}/Dockerfile"
-echo "RUN wget https://dl.google.com/android/repository/android-ndk-${NDK_VERSION}-linux-x86_64.zip && unzip android-ndk-${NDK_VERSION}-linux-x86_64.zip -d /opt && rm android-ndk-${NDK_VERSION}-linux-x86_64.zip" >> "${dir}/Dockerfile"
+echo "RUN wget -nv https://dl.google.com/android/repository/android-ndk-${NDK_VERSION}-linux-x86_64.zip && unzip -q android-ndk-${NDK_VERSION}-linux-x86_64.zip -d /opt && rm android-ndk-${NDK_VERSION}-linux-x86_64.zip" >> "${dir}/Dockerfile"
 echo "RUN echo ANDROID_NDK_PATH=/opt/android-ndk-${NDK_VERSION} >> /etc/environment" >> "${dir}/Dockerfile"
 
 if [ "$DOCKER_REPO" ]; then
